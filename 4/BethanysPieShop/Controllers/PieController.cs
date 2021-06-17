a1d980e8b7beabe23e5c75710b1987c3be3d9463
Sandeep Kandula <sandeep.kandula@in.abb.com>
2021-06-17 22:21:04 +0530

    Creating the List Page

---
+++ 4/BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,27 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            var piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+            return View(piesListViewModel);
+        }
+    }
+}
---
+++ 4/BethanysPieShop/Models/MockPieRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BethanysPieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        private readonly ICategoryRepository categoryRepository = new MockCategoryRepository();
+        public IEnumerable<Pie> AllPies
+        {
+            get
+            {
+                return new List<Pie>
+            {
+                new Pie {PieId = 1, Name = "Strawberry Pie", Price = 15.95M, Category = new Category {CategoryName = "Fruit Pies"}},
+                new Pie {PieId = 2, Name = "Cheese cake", Price = 18.95M, Category = new Category {CategoryName = "Cheese cakes"}},
+                new Pie {PieId = 3, Name = "Rhubarb Pie", Price = 15.95M, Category = new Category {CategoryName = "Fruit pies"}},
+                new Pie {PieId = 4, Name = "Pumpkin Pie", Price = 12.95M, Category = new Category {CategoryName = "Seasonal pies"}},
+            };
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        public Pie GetPieById(int pieId)
+        {
+            return AllPies.FirstOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
